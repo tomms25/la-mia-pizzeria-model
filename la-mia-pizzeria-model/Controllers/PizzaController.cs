9dd2ab5476b3f992f64c2ffa5df0ee205b0832b2
tommasobaccano@gmail.com <tommasobaccano@gmail.com>
2023-05-08 09:40:00 +0200

    Progetto caricato

---
+++ la-mia-pizzeria-model/Controllers/PizzaController.cs
@@ -0,0 +1,29 @@
+﻿using la_mia_pizzeria_model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_model.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Homepage";
+            using var ctx = new PizzaContext();
+
+            var menu = ctx.Pizzas.ToArray();
+            if (!ctx.Pizzas.Any())
+            {
+                ViewData["Message"] = "Nessun risultato trovato";
+            }
+            return View("Index", menu);
+        }
+
+        public IActionResult Show(long id)
+        {
+            using var ctx = new PizzaContext();
+            var menuItem = ctx.Pizzas.Find(id);
+
+            return View(menuItem);
+        }
+    }
+}
---
+++ la-mia-pizzeria-model/Models/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_model.Models
+{
+    public class Pizza
+    {
+        public Pizza(string img, string name, string description, double price)
+        {
+            Img = img;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        [Key]
+        public long Id { get; set; }
+        public string Img { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
